5926b6b4848c89d223844336d284e550b621ab61
snOlga <olga.safonova@outlook.com>
2023-05-28 17:25:30 +0300

    it works for 1 client and with crutch

--- src/main/java/src/ClientServer/Server.java
+++ src/main/java/src/ClientServer/Server.java
@@ -10,8 +10,11 @@ import java.net.*;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.sql.SQLException;
+import java.util.Objects;
 import java.util.Scanner;
+import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ForkJoinPool;
@@ -32,11 +35,21 @@ public class Server
 
     private static final ForkJoinPool pool = new ForkJoinPool();
     private static final int numberOfThreads = Runtime.getRuntime().availableProcessors();
-    private static final ExecutorService service = Executors.newFixedThreadPool(numberOfThreads/2);
+    private static final ExecutorService service = Executors.newFixedThreadPool(numberOfThreads);
+    private static final ExecutorService cashedService = Executors.newCachedThreadPool();
     private static Socket clientSocket = new Socket();
-    public static void setClientSocket(Socket socket)
+    public static void setClientSocket(Socket socket) throws IOException
     {
         clientSocket = socket;
+//        SocketChannel channel = getClientChannel(socket);
+//        channel.configureBlocking(false);
+//        channel.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+        //переписать input/output stream с учЄтом селектора
+    }
+
+    private static SocketChannel getClientChannel(Socket socket)
+    {
+        return socket.getChannel();
     }
 
     public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
@@ -56,22 +69,19 @@ public class Server
 
         while (true)
         {
-            selector.select();
             //clientSocket = serverSocket.accept();
-            pool.submit(ServerFunc.reading(serverSocket));
+            pool.submit(ServerFunc.reading(serverSocket, selector));
 
-            File fileName = new File("outServer.txt");
+            //System.out.println(pool.getPoolSize());
+
+            File file = new File("outServer.txt");
 
             try
             {
                 ObjectInputStream objectInputStream = new ObjectInputStream(clientSocket.getInputStream());
-                //^EOFException
                 BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
 
-                PrintStream out = new PrintStream(new FileOutputStream(fileName));
-                System.setOut(out);
-
-                Scanner scanner = new Scanner(fileName);
+                Scanner scanner = new Scanner(file);
 
                 try
                 {
@@ -80,16 +90,29 @@ public class Server
                     if(responseToClient)
                     {
                         Command command = (Command) objectInputStream.readObject();
-                        command.execute(user);
+                        //assert ServerFunc.execution(command, user) == null;
+                        service.execute(ServerFunc.execution(command, user, file));
                         System.out.println(command);
-                        out.close();
-                        while (scanner.hasNextLine())
+
+                        service.execute(ServerFunc.writing(scanner, writer));
+
+//                        ServerFunc.getLock().lock();
+//                        ServerFunc.getConditionForDisableClient().await();
+//                        ServerFunc.getLock().unlock();
+                        while (!ServerFunc.anotherCheck)
                         {
-                            writer.write(scanner.nextLine() + "@");
+
                         }
-                        writer.newLine();
-                        writer.flush();
+                        ServerFunc.anotherCheck = false;
+
+//                        while (scanner.hasNextLine())
+//                        {
+//                            writer.write(scanner.nextLine() + "@");
+//                        }
+//                        writer.newLine();
+//                        writer.flush();
                     }
+                    Thread.sleep(100);
                 } catch (ClassNotFoundException e)
                 {
                     System.out.println(e.getMessage());
@@ -97,10 +120,15 @@ public class Server
                 {
                     System.out.println("There is no command " + ". For reference, use Ц help");
                 }
+                catch (InterruptedException e)
+                {
+                    throw new RuntimeException(e);
+                }
+                objectInputStream.close();
             }
             catch (SocketException e)
             {
-                System.out.println("Client sent nothing and left.");
+                //System.out.println("Client sent nothing and left.");
             }
             //pool.shutdown();
         }
---
+++ src/main/java/src/CredForPG.java
@@ -0,0 +1,14 @@
+package src;
+
+public class CredForPG {
+    protected static final String name = "***REMOVED***";
+    protected static final String pswd = "***REMOVED***";
+
+    public static String getNameForPG() {
+        return name;
+    }
+
+    public static String getPswdForPG() {
+        return pswd;
+    }
+}
