79f5f6148bcf751c4ec02961a22f671ac4bffc73
snOlga <olga.safonova@outlook.com>
2023-05-28 17:25:30 +0300

    it works for 1 client and with crutch

--- src/main/java/src/ClientServer/ServerFunc.java
+++ src/main/java/src/ClientServer/ServerFunc.java
@@ -3,16 +3,43 @@ package src.ClientServer;
 import src.Command.Command;
 import src.User.User;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.channels.Selector;
 import java.sql.SQLException;
 import java.util.Scanner;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class ServerFunc
 {
-    protected static Runnable reading (ServerSocket serverSocket)
+    private static Lock lock = new ReentrantLock();
+    private static Condition condition = lock.newCondition();
+
+    public static Condition getCondition()
+    {
+        return condition;
+    }
+
+    private static Condition conditionForDisableClient = lock.newCondition();
+
+    public static Condition getConditionForDisableClient()
+    {
+        return conditionForDisableClient;
+    }
+
+    private static boolean check = false;
+    public static boolean anotherCheck = false;
+
+    public static Lock getLock()
+    {
+        return lock;
+    }
+
+    protected static Runnable reading (ServerSocket serverSocket, Selector selector)
     {
         return new Runnable()
         {
@@ -21,6 +48,7 @@ public class ServerFunc
             {
                 try
                 {
+                    selector.select();
                     Server.setClientSocket(serverSocket.accept());
                 } catch (IOException e)
                 {
@@ -30,20 +58,77 @@ public class ServerFunc
         };
     }
 
-    protected static Runnable execution (Command command, User user) throws SQLException, IOException, ClassNotFoundException
+    protected static Runnable execution (Command command, User user, File fileName) throws SQLException, IOException, ClassNotFoundException
     {
-        command.execute(user);
-        return null;
+        return new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                try
+                {
+                    lock.lock();
+                    PrintStream out = new PrintStream(new FileOutputStream(fileName));
+                    System.setOut(out);
+                    command.execute(user);
+                    out.close();
+                    condition.signal();
+                    check = true;
+                    lock.unlock();
+                } catch (IOException | SQLException | ClassNotFoundException e)
+                {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
     }
 
-    protected static Runnable writing (Scanner scanner, BufferedWriter writer) throws IOException
+    protected static Runnable writing (Scanner scanner, BufferedWriter writer) throws InterruptedException
     {
-        while (scanner.hasNextLine())
+        return new Runnable()
         {
-            writer.write(scanner.nextLine() + "@");
-        }
-        writer.newLine();
-        writer.flush();
-        return null;
+            @Override
+            public void run()
+            {
+                lock.lock();
+//                try
+//                {
+//                    condition.await();
+//                    System.out.println("Using");
+//                } catch (InterruptedException e)
+//                {
+//                    throw new RuntimeException(e);
+//                }
+                while (!check)
+                {
+
+                }
+                check = false;
+                while (scanner.hasNextLine())
+                {
+                    try
+                    {
+//                        synchronized (writer)
+//                        {
+                            writer.write(scanner.nextLine() + "@");
+                        //}
+                    } catch (IOException e)
+                    {
+                        throw new RuntimeException(e);
+                    }
+                }
+                try
+                {
+                    writer.newLine();
+                    writer.flush();
+                } catch (IOException e)
+                {
+                    throw new RuntimeException(e);
+                }
+                conditionForDisableClient.signal();
+                anotherCheck = true;
+                lock.unlock();
+            }
+        };
     }
 }
---
+++ src/main/java/src/CredForPG.java
@@ -0,0 +1,14 @@
+package src;
+
+public class CredForPG {
+    protected static final String name = "s368764";
+    protected static final String pswd = "OeC9LaFi3hH1YevG";
+
+    public static String getNameForPG() {
+        return name;
+    }
+
+    public static String getPswdForPG() {
+        return pswd;
+    }
+}
