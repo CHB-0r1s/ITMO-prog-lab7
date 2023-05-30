4c8fb86739a71cae3806184e4ae91cd889a09da0
snOlga <olga.safonova@outlook.com>
2023-05-30 12:34:12 +0300

    a little cleaning

---
+++ src/main/java/src/ClientServer/ClientFunc.java
@@ -0,0 +1,83 @@
+package src.ClientServer;
+
+import src.Command.Command;
+import src.Command.Invoker;
+import src.Multithreading.*;
+import src.User.User;
+import src.User.UserCreator;
+
+import java.io.*;
+import java.net.Socket;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class ClientFunc
+{
+    protected static void sendCommand(Invoker commandInvoker, ObjectOutputStream objectOutputStream, Scanner scanner)
+    {
+        try {
+            while (scanner.hasNextLine()) {
+                Command command = commandInvoker.invokeForClient(scanner.nextLine().trim().split("\s+"));
+                if (command != null)
+                {
+                    objectOutputStream.writeObject(command);
+                    objectOutputStream.flush();
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    protected static void sendUser(ObjectOutputStream objectOutputStream, User user)
+    {
+        try
+        {
+            objectOutputStream.writeObject(user);
+            objectOutputStream.flush();
+        } catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    protected static User readUser(Scanner scanner) throws SQLException, ClassNotFoundException {
+        User user;
+        System.out.println("Do you want to log in or sign up?");
+        while (true)
+        {
+            String logOrSign = scanner.nextLine();
+            if (logOrSign != null && (logOrSign.toLowerCase().contains("log") || logOrSign.toLowerCase().contains("sign")))
+            {
+                if (logOrSign.toLowerCase().contains("log"))
+                {
+                    user = UserCreator.createFromConsole(false);
+                    break;
+                }
+                if (logOrSign.toLowerCase().contains("sign"))
+                {
+                    user = UserCreator.createFromConsole(true);
+                    break;
+                }
+            }
+            else
+            {
+                System.out.println("Incorrect input. Try again.");
+            }
+        }
+        return user;
+    }
+
+    protected static boolean getResponseOfConnecting(BufferedReader reader) throws IOException
+    {
+        return Boolean.parseBoolean(reader.readLine());
+    }
+
+    protected static void readResponse(Socket clientSocket) throws IOException, ClassNotFoundException
+    {
+        ObjectInputStream objectInputStream = new ObjectInputStream(clientSocket.getInputStream());
+        Response response = (Response) objectInputStream.readObject();
+        System.out.println(response.getMessage());
+    }
+}
--- src/main/java/src/ClientServer/Server.java
+++ src/main/java/src/ClientServer/Server.java
@@ -1,6 +1,6 @@
 package src.ClientServer;
-// Да начнутся голодные игры!
 import src.Command.Command;
+import src.Multithreading.*;
 import src.User.User;
 import src.Utils.ManagerOfCollection;
 import src.Utils.PasswordUtils.LoginPasswordManager;
@@ -10,47 +10,16 @@ import java.net.*;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
 import java.sql.SQLException;
-import java.util.Objects;
-import java.util.Scanner;
-import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class Server
 {
-    private static boolean sendResponse(User user, BufferedWriter writer) throws IOException, ClassNotFoundException
-    {
-        boolean response = LoginPasswordManager.compareUser(user);
-
-        writer.write(String.valueOf(response));
-        writer.newLine();
-        writer.flush();
-        return response;
-    }
 
     private static final ForkJoinPool pool = new ForkJoinPool();
     private static final int numberOfThreads = Runtime.getRuntime().availableProcessors();
-    private static final ExecutorService service = Executors.newFixedThreadPool(numberOfThreads);
-    private static final ExecutorService cashedService = Executors.newCachedThreadPool();
-    private static Socket clientSocket = new Socket();
-    public static void setClientSocket(Socket socket) throws IOException
-    {
-        clientSocket = socket;
-//        SocketChannel channel = getClientChannel(socket);
-//        channel.configureBlocking(false);
-//        channel.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
-        //переписать input/output stream с учётом селектора
-    }
-
-    private static SocketChannel getClientChannel(Socket socket)
-    {
-        return socket.getChannel();
-    }
 
     public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
         ManagerOfCollection.createMyCollection();
@@ -67,60 +36,40 @@ public class Server
         serverSocketChannel.configureBlocking(false);
         serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
 
+//        MyConsumer consumer = new MyConsumer();
+//        int numberOfThreads = Runtime.getRuntime().availableProcessors();
+//        ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads);
+//        executorService.submit(consumer);
+
         while (true)
         {
-            //clientSocket = serverSocket.accept();
-            pool.submit(ServerFunc.reading(serverSocket, selector));
-
-            //System.out.println(pool.getPoolSize());
-
-            File file = new File("outServer.txt");
+            selector.select();
+            Socket clientSocket = serverSocket.accept();
 
             try
             {
                 ObjectInputStream objectInputStream = new ObjectInputStream(clientSocket.getInputStream());
                 BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
 
-                Scanner scanner = new Scanner(file);
-
                 try
                 {
                     User user = (User) objectInputStream.readObject();
-                    boolean responseToClient = sendResponse(user, writer);
+                    boolean responseToClient = ServerFunc.sendResponseOfConnecting(user, writer);
+
                     if(responseToClient)
                     {
+//                        ForkJoinPool pool = new ForkJoinPool();
+//                        pool.submit(new Producer(clientSocket));
+
                         Command command = (Command) objectInputStream.readObject();
-                        //assert ServerFunc.execution(command, user) == null;
-                        service.execute(ServerFunc.execution(command, user, file));
-                        System.out.println(command);
-
-                        service.execute(ServerFunc.writing(scanner, writer));
-
-//                        ServerFunc.getLock().lock();
-//                        ServerFunc.getConditionForDisableClient().await();
-//                        ServerFunc.getLock().unlock();
-                        while (!ServerFunc.anotherCheck)
-                        {
-
-                        }
-                        ServerFunc.anotherCheck = false;
-
-//                        while (scanner.hasNextLine())
-//                        {
-//                            writer.write(scanner.nextLine() + "@");
-//                        }
-//                        writer.newLine();
-//                        writer.flush();
+                        String responseLine = ServerFunc.execution(command, user);
+                        ServerFunc.writing(responseLine, clientSocket);
                     }
-                    Thread.sleep(100);
-                } catch (ClassNotFoundException e)
-                {
-                    System.out.println(e.getMessage());
-                } catch (IllegalStateException | NullPointerException | SQLException ex)
+                } catch (IllegalStateException | NullPointerException ex)
                 {
                     System.out.println("There is no command " + ". For reference, use – help");
                 }
-                catch (InterruptedException e)
+                catch (ClassNotFoundException e)
                 {
                     throw new RuntimeException(e);
                 }
@@ -128,9 +77,8 @@ public class Server
             }
             catch (SocketException e)
             {
-                //System.out.println("Client sent nothing and left.");
+                System.out.println("Client sent nothing and left.");
             }
-            //pool.shutdown();
         }
     }
 }
